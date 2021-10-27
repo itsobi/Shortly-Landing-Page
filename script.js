f06907de902a35648d25fbdad53da41f0558f41f
Your Namef <obiobialo@aol.com>
2021-10-26 20:09:16 -0500

    Shortly Landing Page

---
+++ script.js
@@ -0,0 +1,18 @@
+AOS.init({
+    duration: 1000,
+})
+
+// Smooth Scrolling
+// const links = document.querySelectorAll(".nav-ul li a");
+
+// for (const link of links) {
+//     link.addEventListener("click", smoothScroll);
+// }
+
+// function smoothScroll(e) {
+//     e.preventDefault();
+//     const href = this.getAttribute("href");
+//     document.querySelector(href).scrollIntoView({
+//         behaviour: "smooth",
+//     })
+// }
